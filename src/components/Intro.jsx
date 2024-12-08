d1c7f069a14e4ba8c80b4a4320050a67082ea65e
Shantanu Chakrawarty <51011922+shantanuuchak@users.noreply.github.com>
2024-12-08 13:22:09 +0000

    remove {" "}

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,7 +3,7 @@ function Intro() {
   return (
     <>
       <h1 className="text-3xl font-semibold mb-5">
-        Got Ideas? <IconNorthStar className="inline-block" /> We've got <br />{" "}
+        Got Ideas? <IconNorthStar className="inline-block" /> We've got <br />
         the skills. Let's team up.
       </h1>
       <p className="mb-6 text-xl">
