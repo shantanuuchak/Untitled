98d8410b1733c02ba8c618eedf3c3207877879d4
Shantanu Chakrawarty <51011922+shantanuuchak@users.noreply.github.com>
2024-12-08 13:31:04 +0000

    map inputs to state

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,23 @@
+import { useState } from "react";
 import { IconFlareFilled } from "@tabler/icons-react";
 
 function Form() {
+  const [input, setInput] = useState({
+    name: "",
+    email: "",
+    message: "",
+    abilities: [],
+  });
+
+  const helpOptions = [
+    "Website Design",
+    "Content",
+    "UX Design",
+    "Strategy",
+    "User Research",
+    "Other",
+  ];
+
   return (
     <form className="flex flex-col gap-1">
       {/* Full Name Field */}
@@ -10,6 +27,7 @@ function Form() {
         id="user-name"
         placeholder="Your name"
         className="border-b border-stone-700 p-2 md:bg-lime-400 md:placeholder-gray-700"
+        value={input.name}
       />
       {/* Company Field */}
       <input
@@ -18,44 +36,35 @@ function Form() {
         id="user-email"
         placeholder="your@company.com"
         className="border-b border-stone-700 p-2 md:bg-lime-400 md:placeholder-gray-700"
+        value={input.email}
       />
-      {/* Comment */}
+      {/* Message */}
       <input
         type="text"
-        name="user-comment"
-        id="user-comment"
+        name="user-message"
+        id="user-message"
         placeholder="Tell us a little about your project..."
         className="border-b border-stone-700 p-2 h-24 mb-5 md:bg-lime-400 md:placeholder-gray-700"
+        value={input.message}
       />
 
       {/* Checkbox */}
       {/* TODO: Style checkbox background to green on > medium screens */}
       <p className="text-zinc-800 mb-5">How can we help?</p>
       <div className="grid grid-cols-2 md:w-96 mb-8">
-        <label className="flex gap-2 cursor-pointer">
-          <input type="checkbox" className="h-5 w-5" />
-          Website Design
-        </label>
-        <label className="flex gap-2 cursor-pointer">
-          <input type="checkbox" className="h-5 w-5" />
-          Content
-        </label>
-        <label className="flex gap-2 cursor-pointer">
-          <input type="checkbox" className="h-5 w-5" />
-          UX Design
-        </label>
-        <label className="flex gap-2 cursor-pointer">
-          <input type="checkbox" className="h-5 w-5" />
-          Strategy
-        </label>
-        <label className="flex gap-2 cursor-pointer">
-          <input type="checkbox" className="h-5 w-5" />
-          User Research
-        </label>
-        <label className="flex gap-2 cursor-pointer">
-          <input type="checkbox" className="h-5 w-5" />
-          Other
-        </label>
+        {helpOptions.map((option) => (
+          <label
+            key={crypto.randomUUID()}
+            className="flex gap-2 cursor-pointer"
+          >
+            <input
+              type="checkbox"
+              className="h-5 w-5"
+              checked={input.abilities.includes(option)}
+            />
+            {option}
+          </label>
+        ))}
       </div>
 
       <button
